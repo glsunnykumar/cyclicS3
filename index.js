64bc4347ff21762f1e5ac0946fa6ab0a4eb3ac14
glsunnykumar <45039183+glsunnykumar@users.noreply.github.com>
2023-09-09 17:17:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const fs = require('@cyclic.sh/s3fs')(process.env.CYCLIC_BUCKET_NAME)
+
+app.get('/', async (req, res) => {
+    fs.writeFileSync('my_file.txt', new Date().toISOString())
+    return res.send('Hello World!');
+});
+
+app.get('/contents', async (req, res) => {
+    console.log('/contents route')
+    let content = fs.readFileSync('my_file.txt').toString()
+     
+    return res.send(content);
+});
+
+app.listen(3000, () => {});
